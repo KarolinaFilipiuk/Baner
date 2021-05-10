1c626197aec07fdcb1761f3fffeec6c8be83d404
KarolinaFilipiuk <k.filipiuk18@gmail.com>
2021-05-10 13:11:24 +0200

    add an option for shifting with keys

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const slideList = [{
-    img: "img/img1.jpg",
-    text: "Pierwszy tekst",
+    img: "img/spring.jpg",
+    text: "Wiosna",
 }, {
-    img: "img/img2.jpg",
-    text: "Drugi tekst",
+    img: "img/summer.jpg",
+    text: "Lato",
 }, {
-    img: "img/img3.jpg",
-    text: "Trzeci tekst",
+    img: "img/autumn.jpg",
+    text: "Jesień",
+}, {
+    img: "img/winter.jpg",
+    text: "Zima",
 }]
 
 const image = document.querySelector("img.slider");
@@ -14,14 +17,13 @@ const h1 = document.querySelector("h1.slider");
 const dots = [...document.querySelectorAll(".dots span")];
 
 // Interfejs
-const time = 2000;
+const time = 2500;
 let slideActive = 0;
 
 // Implementacja
 const changeDot = () => {
     const indexActiveDot = dots.findIndex(dot => dot.classList.contains("active"))
     dots[indexActiveDot].classList.remove("active");
-
     dots[slideActive].classList.add("active")
 }
 
@@ -33,4 +35,23 @@ const changeSlide = () => {
     changeDot();
 }
 
-setInterval(changeSlide, time);
+const keyChangeSlide = (event) => {
+    if (event.code === "ArrowLeft" || event.code === "ArrowRight") {
+        clearInterval(indexInterval);
+        event.code === "ArrowLeft" ? slideActive-- : slideActive++;
+
+        if (slideActive === slideList.length) {
+            slideActive = 0;
+        } else if (slideActive < 0) {
+            slideActive = slideList.length - 1;
+        }
+
+        image.src = slideList[slideActive].img;
+        h1.textContent = slideList[slideActive].text;
+        changeDot();
+        indexInterval = setInterval(changeSlide, time);
+    }
+}
+
+let indexInterval = setInterval(changeSlide, time);
+window.addEventListener("keydown", keyChangeSlide);
